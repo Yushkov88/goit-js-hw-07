3b7096937175c1f2657d90bf1192c58df1d6dd41
Yushkov88 <yushkov1988@gmail.com>
2022-08-12 22:41:06 +0300

    hw-js7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,46 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const modal = basicLightbox.create(`
+    <img src="" alt="">
+`);
+const modalImage = modal.element().querySelector('img');
+const gallery = document.querySelector('.gallery');
+
+function renderGallery() {
+  const galleryItemsHtml = galleryItems.reduce(
+    (acc, { original, preview, description }) => {
+      const galleryItem = `
+      <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="large-image.jpg"
+            alt="${description}"
+          />
+        </a>
+      </div>
+    `;
+
+      return acc + galleryItem;
+    },
+    '',
+  );
+
+  gallery.insertAdjacentHTML('beforeend', galleryItemsHtml);
+}
+
+gallery.addEventListener('click', event => {
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  event.preventDefault();
+
+  modalImage.src = event.target.parentNode.href;
+  modalImage.alt = event.target.alt;
+  modal.show();
+});
+
+renderGallery();
